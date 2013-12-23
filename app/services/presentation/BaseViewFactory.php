6f5045adc10b56117f8e346413d236682f6464a8
Jan Oris <jan.oris@gmail.com>
2013-12-23 19:59:11 +0100

    Added Presenter convenience methods

--- app/services/presentation/BaseViewFactory.php
+++ app/services/presentation/BaseViewFactory.php
@@ -1,6 +1,8 @@
 <?php
 
 
+use \Robbo\Presenter\PresentableInterface;
+
 class BaseViewFactory {
 
     protected $parameters = array(
@@ -8,24 +10,48 @@ class BaseViewFactory {
         'messageValues'     => array()
     );
 
-    public function notifyUser( $type, $messages )
+    public function notifyUser($type, $messages)
     {
-        $this->addParameter('messageType', $type);
-        $this->addParameter('messageValues', $messages);
+        $this->_addParameter('messageType', $type);
+        $this->_addParameter('messageValues', $messages);
     }
 
-    protected function addParameter($key, $value)
+    protected function _addParameter($key, $value)
     {
         $this->parameters[$key] = $value;
     }
 
-    protected function makeView( $view )
+    protected function _makeView($view)
     {
         if( Auth::check() ) {
-            $this->addParameter('activeUser', Auth::user());
+            $this->_addParameter( 'activeUser', Auth::user() );
         }
 
         return View::make( $view, $this->parameters );
+    }
+
+    protected function _convertObjectsToPresenters($objects)
+    {
+        $results = array();
+        foreach( $objects as $key => $object ) {
+            $results[$key] = $this->_convertObjectToPresenter($object);
+        }
+
+        return $results;
+    }
+
+    protected function _convertObjectToPresenter($object)
+    {
+        if( $object instanceof PresentableInterface ) {
+            return $object->getPresenter();
+        }
+
+        return $object;
+    }
+
+    protected function _isCollection($value)
+    {
+        return is_array($value) || ($value instanceof \Illuminate\Support\Collection);
     }
 
 }
