8bac99e3338141ca35b469bf2f9d116c368dbcb3
Jan Oris <jan.oris@gmail.com>
2014-04-22 08:29:52 +0000

    Changed default machine name for local environment

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,7 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 
-    'local'             => array('YourMachineName'),
+    'local'             => array('ixudra'),
     'production'        => array('ProductionMachineName'),
 
 ));
