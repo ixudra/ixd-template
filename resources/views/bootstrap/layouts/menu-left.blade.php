f50520260c9c8989dd36b0bf87fc578ee6e2b928
Jan Oris <jan.oris@gmail.com>
2015-04-28 12:00:23 +0200

    Updated menu template

--- resources/views/bootstrap/layouts/menu-left.blade.php
+++ resources/views/bootstrap/layouts/menu-left.blade.php
@@ -1,5 +1,5 @@
 <ul class="nav nav-tabs nav-stacked">
     {{--@if( Auth::check() )
-        <li>{!! HTML::linkRoute('admin.projects.index', 'Projects') !!}</li>
+        <li>{!! HTML::linkRoute('admin.projects.index', Translate::recursive('admin.menu.title.index', array('model' => 'project'))) !!}</li>
     @endif--}}
 </ul>
