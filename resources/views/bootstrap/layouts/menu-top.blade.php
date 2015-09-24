5c356a3d74215a01ab3169e2f3af82b89707fd2b
Jan Oris <jan.oris@gmail.com>
2015-09-24 10:29:33 +0200

    Minor changes

--- resources/views/bootstrap/layouts/menu-top.blade.php
+++ resources/views/bootstrap/layouts/menu-top.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
     <div class="navbar-header">
-        <li><a href="#" class="navbar-brand">YourAppName</a></li>
+        <li>{!! HTML::linkRoute('index', 'YourAppName', array('class' => 'navbar-brand')) !!}</li>
     </div>
     <div class="collapse navbar-collapse navbar-ex1-collapse">
         <ul class="nav navbar-nav navbar-right">
