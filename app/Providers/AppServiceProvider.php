cfd550dcef12c9d9f4862e038094195c492fa76f
Jan Oris <jan.oris@gmail.com>
2017-11-03 15:37:44 +0100

    Minor changes and improvements

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -12,6 +12,8 @@ use Ixudra\Generators\GeneratorsServiceProvider;
 use App\Services\Validation\AppValidator;
 
 use Config;
+use Form;
+use HTML;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -22,10 +24,71 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        // Resolve validator
+        /**
+         * Validator
+         */
+
         Validator::resolver(function($translator, $data, $rules, $messages)
         {
             return new AppValidator( $translator, $data, $rules, $messages );
+        });
+
+
+        /**
+         * HTML macros
+         */
+
+        Form::macro('openFormGroup', function($name = '', $errors = null, $requiredFields = array(), $showErrors = true, $showRequired = true)
+        {
+            $style = '';
+            if( $showErrors && !is_null($errors) && $errors->has( $name ) ) {
+                $style .=' has-error';
+            }
+
+            if( $showRequired && in_array( $name, $requiredFields ) ) {
+                $style .=' required';
+            }
+
+            return '<div class="form-group '. $style .'">';
+        });
+
+        Form::macro('closeFormGroup', function($name = '', $errors = null, $showErrors = true, $offsetLeft = 3)
+        {
+            $output = '';
+            if( $showErrors && !is_null($errors) && $errors->has( $name ) ) {
+                $output .= '<div class="col-lg-12">';
+                if( $offsetLeft != 0 ) {
+                    $output .= '<div class="col-lg-3">&nbsp;</div>'
+                        . '<div class="col-lg-8">';
+                }
+
+                $output .= $errors->first($name, '<span class="help-block">:message</span>');
+
+                if( $offsetLeft != 0 ) {
+                    $output .=  '</div>';
+                }
+
+                $output .=  '</div>';
+            }
+
+            return $output .'</div>';
+        });
+
+        Form::macro('iconSubmit', function($value, $iconType, $options = array())
+        {
+            $icon = '<i class="glyphicon glyphicon-'. $iconType .'" aria-hidden="true"></i>&nbsp;'. $value;
+            $options[ 'type' ] = 'submit';
+            $link = $this->button('#', $options);
+
+            return str_replace('#', $icon, $link);
+        });
+
+        HTML::macro('iconRoute', function($route = '', $data, $iconType, $parameters = array(), $attributes = array())
+        {
+            $icon = '<i class="glyphicon glyphicon-'. $iconType .'" aria-hidden="true"></i>&nbsp;';
+            $link = HTML::linkRoute($route, '#'. $data, $parameters, $attributes);
+
+            return str_replace('#', $icon, $link);
         });
     }
 
--- createDatabaseDumps.sh
+++ createDatabaseDumps.sh
@@ -1,7 +1,14 @@
 
-	echo "Creating database dumps... "
+    echo "Creating database dumps... "
 
-	mysqldump -u root -proot --no-data ixd_yourAppName > tests/_data/dump.sql;
-	mysqldump -u root -proot --no-data ixd_yourAppName > tests/_data/api-dump.sql;
-	mysqldump -u root -proot --no-data ixd_yourAppName > tests/_data/acceptance-dump.sql;
+    # Set up fresh database with empty tables
+    php artisan migrate:fresh
+
+    # Create database dumps
+    mysqldump -u root -proot --no-data ixd_easy_office > tests/_data/dump.sql;
+    mysqldump -u root -proot --no-data ixd_easy_office > tests/_data/api-dump.sql;
+    mysqldump -u root -proot --no-data ixd_easy_office > tests/_data/acceptance-dump.sql;
+
+    # Set up fresh database with seed data
+    php artisan migrate:fresh --seed
 
